6433d6cb95295539e6352d09e67be588121fbdbb
Rusha Thapa <rushathapa71@gmail.com>
2024-04-08 11:35:19 +0545

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert("heyy 3+3") //we are using nodejs, not browser
+
+console.log(347389
+    +
+    33); //code readability should be high
+
+    let name = "rusha"
+    let age = 16
+    let isLoggedIn = false
+
+    //number => 2 to the power 53
+    // bigint - ekdam dherai data use hune bela
+    //string =>""
+    //boolean => true or false
+    //null => standalone value, hamile aafai assign gareko
+    //undefined => variable define garyo tara value deko chaina
+    //symbol => unique
+    //all above mentioned data types are primitive datatypes. 
+
+
+    //object
+    console.log(typeof "mountain mug")
+    console.log(typeof age)
+    console.log(typeof null) //object 
+    console.log(typeof undefined) //undefined
