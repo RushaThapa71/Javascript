00c78cc33c065b80db5fa879fe32dd37e27080c4
Rusha Thapa <rushathapa71@gmail.com>
2024-04-08 11:51:54 +0545

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let scores = "Rusha"
+
+console.log(typeof scores);
+console.log(typeof (scores))
+
+let valueInNumber = Number(scores)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+//"32" => "32"
+//"32abc" => NaN
+//true  => 1; false=> 0
+
+let isLoggedIn = false
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 = true; 0 = false
+//"" = false
+//"rusha" = true
+
+let someNumber = 99
+let stringNumber = String(someNumber)
+console.log(stringNumber)
