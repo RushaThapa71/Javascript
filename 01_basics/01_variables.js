f496062fd859f1089ffdd372060cd6c1a2daff55
Rusha Thapa <rusha.thapa@citytech.global>
2024-04-08 10:11:03 +0545

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 15464
+let accountEmail = "rushathapa71@gmial.com"
+var accountPassword = "15484"
+accountCity = "Catmandooo"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "653463444444"
+accountCity = "basasntapur"
+
+/*
+Prefer not to use var
+because of block and functional scope
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
