942d29b2beba598d1a457f2b7face105e3ef5857
Rusha Thapa <rushathapa71@gmail.com>
2024-04-09 11:18:59 +0545

    conversion operations completed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,39 @@ console.log(booleanIsLoggedIn)
 let someNumber = 99
 let stringNumber = String(someNumber)
 console.log(stringNumber)
+
+
+//******************OPERATIONS****************
+let value = 99
+let negValue = -value
+console.log(value)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**5) // 2**5 = 2 to the power 5
+console.log(2/2)
+console.log(2%3)
+
+let str1 = "hello"
+let str2 = " rusha"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2) //12
+console.log(1 + "2") //12
+console.log("1" + "2") //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
+
+// Bad code practice
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3, num4
+num1 = num2 = num3 = num4 = 6+4
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
